e2199a8ad1171e5657d4703a77b461ff8bf2588a
Michael Langbein <michael.langbein@dlr.de>
2019-05-27 16:10:56 +0200

    changed plot to seaborn

--- print.py
+++ print.py
@@ -1,158 +1,22 @@
 import pickle
-import matplotlib
-import matplotlib.pyplot as plt
 import numpy as np
-
-
-def heatmap(data, row_label, row_labels, col_label, col_labels, ax=None,
-            cbar_kw={}, cbarlabel="", **kwargs):
-    """
-    Create a heatmap from a numpy array and two lists of labels.
-
-    Parameters
-    ----------
-    data
-        A 2D numpy array of shape (N, M).
-    row_label
-        An overal row-label
-    row_labels
-        A list or array of length N with the labels for the rows.
-    col_label
-        An overal col-label
-    col_labels
-        A list or array of length M with the labels for the columns.
-    ax
-        A `matplotlib.axes.Axes` instance to which the heatmap is plotted.  If
-        not provided, use current axes or create a new one.  Optional.
-    cbar_kw
-        A dictionary with arguments to `matplotlib.Figure.colorbar`.  Optional.
-    cbarlabel
-        The label for the colorbar.  Optional.
-    **kwargs
-        All other arguments are forwarded to `imshow`.
-    """
-
-    if not ax:
-        ax = plt.gca()
-
-    # Plot the heatmap
-    im = ax.imshow(data, **kwargs)
-
-    # Create colorbar
-    cbar = ax.figure.colorbar(im, ax=ax, **cbar_kw)
-    cbar.ax.set_ylabel(cbarlabel, rotation=-90, va="bottom")
-
-    # labels
-    ax.set_xlabel(col_label)
-    ax.set_ylabel(row_label)
-
-    # We want to show all ticks...
-    ax.set_xticks(np.arange(data.shape[1]))
-    ax.set_yticks(np.arange(data.shape[0]))
-    # ... and label them with the respective list entries.
-    ax.set_xticklabels(col_labels)
-    ax.set_yticklabels(row_labels)
-
-    # Let the horizontal axes labeling appear on top.
-    ax.tick_params(top=True, bottom=False,
-                   labeltop=True, labelbottom=False)
-
-    # Rotate the tick labels and set their alignment.
-    plt.setp(ax.get_xticklabels(), rotation=-30, ha="right",
-             rotation_mode="anchor")
-
-    # Turn spines off and create white grid.
-    for edge, spine in ax.spines.items():
-        spine.set_visible(False)
-
-    ax.set_xticks(np.arange(data.shape[1]+1)-.5, minor=True)
-    ax.set_yticks(np.arange(data.shape[0]+1)-.5, minor=True)
-    ax.grid(which="minor", color="w", linestyle='-', linewidth=3)
-    ax.tick_params(which="minor", bottom=False, left=False)
-
-    return im, cbar
-
-
-def annotate_heatmap(im, data=None, valfmt="{x:.2f}",
-                     textcolors=["black", "white"],
-                     threshold=None, **textkw):
-    """
-    A function to annotate a heatmap.
-
-    Parameters
-    ----------
-    im
-        The AxesImage to be labeled.
-    data
-        Data used to annotate.  If None, the image's data is used.  Optional.
-    valfmt
-        The format of the annotations inside the heatmap.  This should either
-        use the string format method, e.g. "$ {x:.2f}", or be a
-        `matplotlib.ticker.Formatter`.  Optional.
-    textcolors
-        A list or array of two color specifications.  The first is used for
-        values below a threshold, the second for those above.  Optional.
-    threshold
-        Value in data units according to which the colors from textcolors are
-        applied.  If None (the default) uses the middle of the colormap as
-        separation.  Optional.
-    **kwargs
-        All other arguments are forwarded to each call to `text` used to create
-        the text labels.
-    """
-
-    if not isinstance(data, (list, np.ndarray)):
-        data = im.get_array()
-
-    # Normalize the threshold to the images color range.
-    if threshold is not None:
-        threshold = im.norm(threshold)
-    else:
-        threshold = im.norm(data.max())/2.
-
-    # Set default alignment to center, but allow it to be
-    # overwritten by textkw.
-    kw = dict(horizontalalignment="center",
-              verticalalignment="center")
-    kw.update(textkw)
-
-    # Get the formatter in case a string is supplied
-    if isinstance(valfmt, str):
-        valfmt = matplotlib.ticker.StrMethodFormatter(valfmt)
-
-    # Loop over the data and create a `Text` for each "pixel".
-    # Change the text's color depending on the data.
-    texts = []
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            kw.update(color=textcolors[int(im.norm(data[i, j]) > threshold)])
-            text = im.axes.text(j, i, valfmt(data[i, j], None), **kw)
-            texts.append(text)
-
-    return texts
-
-
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
 
 
 results = pickle.load( open("results_second.pkl", "rb") )
+data = pd.DataFrame.from_dict(results).applymap(lambda element: element["val_loss"][-1])
+#data = data.applymap(lambda val: np.log(val))
 
-
-data = []
-dataLabels = []
-netwLabels = []
-for dataLabel in results: 
-    dataLabels.append(dataLabel)
-    netwLabels.append(dataLabel)
-    row = []
-    for netwLabel in results[dataLabel]:
-        entry = results[dataLabel][netwLabel]["val_loss"][-1]
-        row.append( entry )
-    data.append(row)
-data = np.array(data)
-
-
-fig, ax = plt.subplots()
-im, cbar = heatmap(data, "data", dataLabels, "networks", netwLabels, ax=ax, cmap="RdYlGn_r", cbarlabel="approximation error")
-texts = annotate_heatmap(im, valfmt="{x:.3f}")
-fig.tight_layout()
+fig = plt.figure(figsize=(20,15))
+ax1 = plt.subplot2grid((20,20), (0,1), colspan=19, rowspan=19) # main
+ax2 = plt.subplot2grid((20,20), (19,1), colspan=19, rowspan=1) # bottom
+ax3 = plt.subplot2grid((20,20), (0,0), colspan=1, rowspan=19) # left
+ax3.set(ylabel="data")
+ax2.set(xlabel="network")
+sb.heatmap(data,                                        ax=ax1,  annot=True, cbar=False, xticklabels=False, yticklabels=False,  cmap="RdYlGn_r") # main
+sb.heatmap(pd.DataFrame(data.sum(axis=0)).transpose(),  ax=ax2,  annot=True, cbar=False, xticklabels=True, yticklabels=False,   cmap="RdYlGn_r") # bottom
+sb.heatmap(pd.DataFrame(data.sum(axis=1)),              ax=ax3,  annot=True, cbar=False, xticklabels=False, yticklabels=True,   cmap="RdYlGn_r") # left
 plt.show()
+
